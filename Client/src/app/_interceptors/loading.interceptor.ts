c83401e7bdd2752350d10507607b47f840842d51
Trung Duong <pdtduong@gmail.com>
2022-03-12 15:25:58 +0700

    commit "publishing"

--- Client/src/app/_interceptors/loading.interceptor.ts
+++ Client/src/app/_interceptors/loading.interceptor.ts
@@ -16,7 +16,6 @@ export class LoadingInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     this.busyService.busy();
     return next.handle(request).pipe(
-      delay(0),
       finalize(() => {
         this.busyService.idle();
       })
